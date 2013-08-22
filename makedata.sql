8791cbe1988508a631e27b674e967a8ce85b937d
Andrius K <akvara@gmail.com>
2013-08-22 16:22:48 +0300

    With makedata

---
+++ makedata.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE `stickynotes` DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci; 
+USE `stickynotes`;
+CREATE USER 'www-data'@'localhost' IDENTIFIED BY 'www-data';
+GRANT ALL PRIVILEGES ON `stickynotes`.* TO 'www-data'@'localhost';
+
+DROP TABLE IF EXISTS `stickynotes`.`stickynotes` ;
+CREATE TABLE IF NOT EXISTS `stickynotes`.`stickynotes` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`note` VARCHAR(255) NULL ,
+`created` TIMESTAMP NOT NULL ,
+PRIMARY KEY (`id`) ,
+UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
+ENGINE = MyISAM;
+-- -----------------------------------------------------
+-- Data for table `stickynotes`.`stickynotes`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `stickynotes`;
+INSERT INTO `stickynotes`.`stickynotes` (`id`, `note`, `created`) VALUES (NULL, 'This is a sticky note you can type and edit.', NULL);
+INSERT INTO `stickynotes`.`stickynotes` (`id`, `note`, `created`) VALUES (NULL, 'Lets see if it will work with my iPhone', NULL);
+COMMIT;
